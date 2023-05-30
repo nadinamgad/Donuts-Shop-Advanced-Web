002eb1ef40dc5842b4b4056b97ee61817e3807b0
nadinamgad <nadeen.amgad@msa.edu.eg>
2023-05-30 20:09:52 +0300

    update IndexPage path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import IndexPage from "./Components/indexPage";
+import IndexPage from "./Components/IndexPage";
 // import NavBar from './Components/NavBar.jsx';
 
 function App() {
