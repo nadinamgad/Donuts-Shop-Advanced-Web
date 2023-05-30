bcc518541d45a813dcdb9b7ccd5c91d4f69d60c1
nadinamgad <nadeen.amgad@msa.edu.eg>
2023-05-30 17:38:56 +0300

    Adjusted slider buttons position

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -3,9 +3,9 @@ import "../App.css";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "../Style/Home.css";
 
-import slide2 from "../assets/nardeena.png";
+import slide2 from "../assets/vanilla_tower2.png";
 import slide3 from "../assets/chocolate_aesthetic_donuts-removebg-preview.png";
-import "../Style/ImageSlider.css"
+import "../Style/ImageSlider.css";
 
 export default function Home() {
   const [index, setIndex] = useState(0);
@@ -26,38 +26,47 @@ export default function Home() {
           <h1 className="main-text">Vanilla</h1>
         </Col>
       </Row> */}
-      
+
       <Row className="text-row">
         <Col>
           <h1>
-            <span className="el_tetr" id="gradient-text">Donut store,</span>
+            <span className="el_tetr" id="gradient-text">
+              Donut store,
+            </span>
           </h1>
           <h1>
-            <span className="sub-el-tetr" id="gradient-text2">like never before</span>
+            <span className="sub-el-tetr" id="gradient-text2">
+              like never before
+            </span>
           </h1>
+        </Col>
+
+        <Col xs="auto">
+          <button onClick={previous} className="sliders-button">
+            Previous
+          </button>
         </Col>
 
         <Col>
           <div className="slider">
             <div className="slideshow">
               <img src={images[index]} className="mainimg" alt="slide" />
-              <button onClick={previous}>Previous</button>
-              <button onClick={next}>Next</button>
             </div>
           </div>
         </Col>
 
-      </Row>
-      
-      <Row>
-        <Col className="button-col">
-          <Button className="order-now">
-            Order Now
-          </Button>
+        <Col xs="auto">
+          <button onClick={next} className="sliders-button">
+            Next
+          </button>
         </Col>
       </Row>
 
-
+      <Row>
+        <Col className="button-col">
+          <Button className="order-now">Order Now</Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
