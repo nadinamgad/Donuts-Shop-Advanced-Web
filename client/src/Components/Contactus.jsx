f4e0da1afb4e18cd6980ccd955d91c5a595a3c9e
Nathalie <nathalieamgad80@gmail.com>
2023-06-01 20:46:31 +0200

    added contact us page

--- client/src/Components/Contactus.jsx
+++ client/src/Components/Contactus.jsx
@@ -1,5 +1,40 @@
+import React from 'react'
+import "../Style/ContactUs.css"
+
 export default function Contactus() {
     
-    return <h1> Here should be the contact us page </h1>
+    return(
+        <div className="Container"> 
+        
+        <h1 className="contactTitle">  Feel free to contact us.</h1>
+
+        <div className='form-container'>
+        <form>
+            <div className="Name">
+                <label for="firstname"> First Name </label>
+                <input type="text" name="firstName" className="firstName" placeholder="enter first name" tabIndex="1"/>
+
+                <label for="lastname"> Last Name </label>
+                <input type="text" name="lastName" className="lastName" placeholder="enter last name" tabIndex="2"/>
+
+                <label for="email"> Email </label>
+                <input type="email" name="email" className="email" placeholder="example@corp.com" tabIndex="3"/>
+            
+                <label for="message"> Message </label>
+                <textarea placeholder="Start typing..." className="message" name="message"/>
+     
+                <button type="submit" className="send"> Send </button>
+            </div>
+
+            <div className="links">
+                <a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
+                <a href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
+                <a href="https://www.facebook.com/"><i className="fab fa-facebook"></i></a>
+            </div>
+        </form>
+        </div>
+       
+        </div>
+    )
 
 }
