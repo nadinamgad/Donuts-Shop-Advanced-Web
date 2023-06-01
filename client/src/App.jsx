47c449f93ee6ec49fbb96f1b8cf075e4670de6be
nadinamgad <nadeen.amgad@msa.edu.eg>
2023-06-02 00:42:07 +0300

    Added Dark mode (need edits)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,26 @@ import AboutUs from "./Components/AboutUs";
 import Layout from "./Components/Layout";
 import Home from "./Components/Home";
 import Contactus from "./Components/Contactus";
+import { useState } from "react";
 // import NavBar from './Components/NavBar.jsx';
 
 function App() {
+  const [Mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    setMode(prevMode => (prevMode === "light" ? "dark" : "light"));
+    console.log("heloo!")
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
+      <Route path="/" element={<Layout mode={Mode} toggleMode={toggleMode} />}>
         <Route index element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contact" element={<Contactus/>} />
       </Route>
     </Routes>
 
-    // <div>
-    //   <p style={{ color: 'red' }}> hi </p>
-    // </div>
   );
 }
 
--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 
-export default function Layout() {
+export default function Layout({mode, toggleMode}) {
     return (
-        <div>
-            <NavBar />
+        <div className={mode === "dark" ? "dark-mode" : "light-mode"}>
+            <NavBar toggleMode={toggleMode}/>
             <Outlet/>
         </div>
     )
