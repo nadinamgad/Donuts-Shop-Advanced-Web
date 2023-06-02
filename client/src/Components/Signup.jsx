1da129b6b8269b69bb760d4e44344ab414f88060
Nathalie <nathalieamgad80@gmail.com>
2023-06-03 01:09:10 +0200

    edited Login and signup

--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -17,22 +17,22 @@ export const Signup = (props) => {
         <h1 className="Signup"> Sign-up </h1>
         <form className="Signup-form" onSubmit={handleSubmit}>
            
-            <input type={name} placeholder="Enter first name" id="name" name="name"/>
+            <input type={name} placeholder="Enter first name" id="name" name="name"required/>
 
-            <input type={name} placeholder="Enter last name" id="name" name="name"/>
+            <input type={name} placeholder="Enter last name" id="name" name="name"required/>
 
-            <input  type="tel" placeholder="Enter phone number" id="number" name="number"/>
+            <input  type="tel" placeholder="Enter phone number" id="number" name="number"required/>
 
-           <input value ={email} type="email" placeholder="Enter email" id="email" name="email"/>
-      
+           <input value ={email} type="email" placeholder="Enter email" id="email" name="email"required/>
           
-           <input value ={pass} type="password" placeholder="Enter password" id="pasaword" name="password"/>
-
-           <input value ={pass} type="password" placeholder="Confirm password" id="pasaword" name="password"/>
+           <input value ={pass} type="password" placeholder="Enter password" id="pasaword" name="password"required/>
+          
+           <input value ={pass} type="password" placeholder="Confirm password" id="pasaword" name="password"required/>
       
            <button type="submit"> Signup </button>
         </form>
 
+       
         <button className="link-btn" onClick={() => props.onFormSwitch('Login')}> Already have an account? <b> Login here. </b> </button>
 
      </div>
